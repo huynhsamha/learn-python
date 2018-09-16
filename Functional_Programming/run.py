76f2b71614adedc4b8d3e9416f8068d204be8e84
Huynh Ha <huynhsamha@gmail.com>
2018-09-16 15:41:30 +0700

    update Functional Programming

--- Functional_Programming/run.py
+++ Functional_Programming/run.py
@@ -2,18 +2,6 @@
 Document:
 """
 
+# This file only for running
 
-import functools
-import operator
-
-lmax = lambda xs: functools.reduce(lambda x, y: x if x > y else y, xs)
-lmin = lambda xs: functools.reduce(lambda x, y: x if x < y else y, xs)
-
-lsum = lambda xs: functools.reduce(operator.add, xs)
-lmul = lambda xs: functools.reduce(operator.mul, xs)
-
-lany = lambda pred, xs: functools.reduce(lambda x, y: x or pred(y), xs, False)
-lall = lambda pred, xs: functools.reduce(lambda x, y: x and pred(y), xs, True)
-
-lmap = lambda func, xs: functools.reduce(lambda x, y: x + [func(y)], xs, [])
-lfilter = lambda func, xs: functools.reduce(lambda x, y: x + [y] if func(y) else x, xs, [])
+print('Hello Python')
