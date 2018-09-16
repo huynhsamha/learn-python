828c378befc11620af6ecefdb39bbfd3947f0cb3
Huynh Ha <huynhsamha@gmail.com>
2018-09-16 10:51:11 +0700

    update Functional Programming

--- Functional_Programming/partial.py
+++ Functional_Programming/partial.py
@@ -1,19 +1,3 @@
-"""
-Document:
-
-"""
-
-
-animals = [
-    {'type': 'Dog', 'name': 'Lucy'},
-    {'type': 'Cat', 'name': 'Buddy'},
-    {'type': 'Rabbit', 'name': 'Jack'},
-    {'type': 'Cat', 'name': 'Duke'},
-    {'type': 'Rabbit', 'name': 'Sadie'},
-    {'type': 'Dog', 'name': 'Bella'},
-]
-
-
 import math
 import functools
 
